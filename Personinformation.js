bc4713bd9de6af9f8f1bfb45a3aeb5b29f0d0763
GeetaSMath <geetasmath@gmail.com>
2021-12-07 18:08:13 +0530

    Added: validate contact

--- Personinformation.js
+++ Personinformation.js
@@ -1,105 +1,126 @@
-firstName; lastName; address; city; state; zip; phoneNumber; email;
+class PersonInformation {
 
-//constructor
-constructor ( firstName, lastName, address, city, state, zip, phoneNumber, email ) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.address = address;
-    this.city = city;
-    this.state = state;
-    this.zip = zip;
-    this.phoneNumber = phoneNumber;
-    this.email = email;
+    //property
+    firstName; lastName; address; city; state; zip; phoneNumber; email;
+
+    //setter and getter method
+    set firstName( firstName ) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if ( nameRegex.test( firstName )) {
+            this.firstName = firstName;
+        }
+        else {
+            throw "First Name is Incorrect!";
+        }
+    }
+
+    get firstName() {
+        return this.firstName;
+    }
+
+    set lastName( lastName ) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if ( nameRegex.test( lastName )) {
+            this.lastName = lastName;
+        }
+        else {
+            throw "Last Name is Incorrect!";
+        }
+    }
+
+    get lastName() {
+        return this.lastName;
+    }
+
+    set address( address ) {
+        let addRegex = RegExp('^([0-9A-Za-z\\s,-\\\\.]{4,})$');
+        if ( addRegex.test( address )) {
+            this.address = address;
+        }
+        else {
+            throw "Address is Incorrect!";
+        }
+    }
+
+    get address() {
+        return this.address;
+    }
+
+    set city( city ) {
+        let cityRegex = RegExp('^([A-Za-z\\s]{4,})$');
+        if ( cityRegex.test( city )) {
+            this.city = city;
+        }
+        else {
+            throw "City is Incorrect!";
+        }
+    }
+
+    get city() {
+        return this.city;
+    }
+
+    set state( state ) {
+        let stateRegex = RegExp('^([A-Za-z\\s]{4,})$');
+        if ( stateRegex.test( state )) {
+            this.state = state;
+        }
+        else {
+            throw "State is Incorrect!";
+        }
+    }
+
+    get state() {
+        return this.state;
+    }
+
+    set zip( zip ) {
+        let zipRegex = RegExp('^([0-9]{6})|([0-9]{3}(\\s)[0-9]{3})$');
+        if ( zipRegex.test( zip )) {
+            this.zip = zip;
+        }
+        else {
+            throw "Zip is Incorrect!";
+        }
+    }
+
+    get zip() {
+        return this.zip;
+    }
+
+    set phoneNumber( phoneNumber ) {
+        let phoneRegex = RegExp('^(0|91)?[\\s][0-9]{10}$');
+        if ( phoneRegex.test( phoneNumber )) {
+            this.phoneNumber = phoneNumber;
+        }
+        else {
+            throw "Phone Number is Incorrect!";
+        }
+    }
+
+    get phoneNumber() {
+        return this.phoneNumber;
+    }
+
+    set email( email ) {
+        let emailRegex = RegExp('^[A-Za-z0-9!#$%&*+\\\\=?`{|}~^-]+(?:\\.[A-Za-z!#$%&*+\\\\=?`{|}~^-]+)*@(?:([0-9-]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}$');
+        if ( emailRegex.test( email )) {
+            this.email = email;
+        }
+        else {
+            throw "Email is Incorrect!";
+        }
+    }
+
+    get email() {
+        return this.email;
+    }
+
+    //method
+    toString() {
+        return "firstName = " +this.firstName + ", lastName = " +this.lastName + ", address = " +this.address + ", city = " +this.city + ", state = " +this.state + ", zip = " +this.zip + ", phoneNumber = " +this.phoneNumber + ", email = " +this.email;
+    }
 }
-
-//setter and getter method
- setfirstName( firstName )
-  {
-    this.firstName = firstName;
-}
-
- getfirstName() 
- {
-    return this.firstName;
-}
-
- setlastName( lastName )
-  {
-    this.lastName = lastName;
-}
-
- getlastName() 
- {
-    return this.lastName;
-}
-
- setaddress( address )
-  {
-    this.address = address;
-}
-
- getaddress()
-  {
-    return this.address;
-}
-
- setcity( city )
-  {
-    this.city = city;
-}
-
- getcity() 
- {
-    return this.city;
-}
-
- setstate( state )
-  {
-    this.state = state;
-}
-
-getstate()
- {
-    return this.state;
-}
-
-setzip( zip ) 
-{
-    this.zip = zip;
-}
-
-getzip()
- {
-    return this.zip;
-}
-
-setphoneNumber( phoneNumber )
- {
-    this.phoneNumber = phoneNumber;
-}
-
-getphoneNumber()
- {
-    return this.phoneNumber;
-}
-
-setemail( email )
- {
-    this.email = email;
-}
-
-getemail()
- {
-    return this.email;
-}
-
-//method
-toString()
- {
-    return "firstName = " +this.firstName + ", lastName = " +this.lastName + ", address = " +this.address + ", city = " +this.city +
-                            ", state = " +this.state + ", zip = " +this.zip + ", phoneNumber = " +this.phoneNumber + ", email = " +this.email;
-}
-
 
 //exporting the class file to another class
 module.exports = PersonInformation;
